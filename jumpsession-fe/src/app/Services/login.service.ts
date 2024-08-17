d26653fbece3f46de1a7f141cf017e73612e0045
CameronPadiachy <cameronpadiachy@icloud.com>
2024-08-17 13:22:35 +0200

    Login

--- jumpsession-fe/src/app/Services/login.service.ts
+++ jumpsession-fe/src/app/Services/login.service.ts
@@ -2,27 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Admin } from '../Models/Admin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:8080/api/admin';
+  private apiUrl = 'http://localhost:8080/api/login';
 
   constructor(private http: HttpClient) { }
 
   //ToDo: write Login endpoint to handle log in, leave this as is for them to fix with correct endpoint, Maybe
-  login(username: string, password: string): Observable<boolean> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  // login(username: string, password: string): Observable<boolean> {
+
     
-    return this.http.get<any>(`${this.apiUrl}/get-admins`, { headers })
-      .pipe(map(response => {
-        for (let admin of response) {
-          if (admin.username === username && admin.password === password) {
-            return true;
-          }
-        }
-        return false;
-      }));
+  //   return this.http.get<boolean>(`${this.apiUrl}/post-login_details`, { headers })
+  //     .pipe(map(response => {
+  //       for (let admin of response) {
+  //         if (admin.username === username && admin.password === password) {
+  //           return true;
+  //         }
+  //       }
+  //       return false;
+  //       console.log(response);
+  //       return response.status;
+  //     }));
+  // }
+
+
+  checkdetail(admin: Admin){
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<boolean>(`${this.apiUrl}/post-login_details`, admin, {headers }  )
+      
   }
 }
