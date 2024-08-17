b28cb8ec3002f9f2c6fba00ba30348d8c56b6c4e
Shar-06 <sharlenemoetjie18@gmail.com>
2024-08-17 14:01:03 +0200

    add book works

--- jumpsession-fe/src/app/Models/book.ts
+++ jumpsession-fe/src/app/Models/book.ts
@@ -2,5 +2,5 @@ export interface Book {
     id: number,
     book_name: string;
     isbn_number: string;
-    author: string;  
+    author: string;
 }
--- jumpsession-fe/src/app/Services/books.service.ts
+++ jumpsession-fe/src/app/Services/books.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Book } from '../Models/book';
@@ -24,6 +24,21 @@ export class BooksService {
         catchError(error => {
           console.error('Error fetching users:', error);
           return throwError('Error fetching users, please try again later.');
+        })
+      );
+  }
+
+  addBook(book:Book): Observable<Book[]>{
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+    return this.httpClient.post<Book[]>(`${this.apiUrl}/create-book`,book, { headers })
+      .pipe(
+        map(response => {
+          return response; // Assuming the response is an array of books
+        }),
+        catchError(error => {
+          console.error('Error adding book:', error);
+          return throwError('Error adding book, please try again later.');
         })
       );
   }
