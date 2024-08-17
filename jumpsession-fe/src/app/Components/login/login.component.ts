72dce6883d8bc25a87f702be023841a989213608
SechabaNtshehi <146662908+SechabaNtshehi@users.noreply.github.com>
2024-08-17 12:27:18 +0200

    FE BE connection
    Connected FE to BE on the login

--- jumpsession-fe/src/app/Components/login/login.component.ts
+++ jumpsession-fe/src/app/Components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import Login from 'src/app/Models/login';
 import { LoginService } from 'src/app/Services/login.service';
 import { NavService } from 'src/app/Shared-Services/nav.service';
 
@@ -31,10 +32,20 @@ export class LoginComponent {
     //     this.errorMessage = 'An error occurred. Please try again.';
     //   }
     // );
-    this.loginService.login(this.username, this.password).subscribe({
-      next: () => {
-        this.router.navigateByUrl('/users');
-        this.navService.toggleNav();
+    let loginObj: Login = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.loginService.login(loginObj).subscribe(
+      {
+      next: (res: boolean) => {
+        if (res) {
+          this.router.navigateByUrl('/users');
+          this.navService.toggleNav();
+        } else {
+          this.errorMessage = 'Username and password are incorrect.';
+        }
       },
       error: () => {
         this.errorMessage = 'An error occurred. Please try again.';
--- jumpsession-fe/src/app/Services/login.service.ts
+++ jumpsession-fe/src/app/Services/login.service.ts
@@ -2,27 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import Login from '../Models/login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:8080/api/admin';
+  private apiUrl = 'http://localhost:8080/api/login';
 
   constructor(private http: HttpClient) { }
 
   //ToDo: write Login endpoint to handle log in, leave this as is for them to fix with correct endpoint, Maybe
-  login(username: string, password: string): Observable<boolean> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  // login(username: string, password: string): Observable<boolean> {
+  //   const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     
-    return this.http.get<any>(`${this.apiUrl}/get-admins`, { headers })
-      .pipe(map(response => {
-        for (let admin of response) {
-          if (admin.username === username && admin.password === password) {
-            return true;
-          }
-        }
-        return false;
-      }));
+  //   return this.http.get<any>(`${this.apiUrl}/post-login_details`, { headers })
+  //     .pipe(map(response => {
+  //       // for (let admin of response) {
+  //       //   if (admin.username === username && admin.password === password) {
+  //       //     return true;
+  //       //   }
+  //       // }
+  //       console.log("Hello");
+  //       return /*response.status;*/ false;
+  //     }));
+  // }
+
+  login(login: Login): Observable<boolean>{
+    return this.http.post<boolean>(`${this.apiUrl}/post-login_details`,login);
   }
 }
